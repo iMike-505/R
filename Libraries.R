6b2e07ad362338bc199ff36c3ccc968a10e0f3e4
Miguel Contreras <79333663+iMike-505@users.noreply.github.com>
2022-06-10 23:12:04 -0500

    Add files via upload

---
+++ Libraries.R
@@ -0,0 +1,9 @@
+abs(-98) #Valor absoluto
+
+install.packages("lubridate") #Instalar biblioteca en el código
+
+library(dplyr) #Forma para cargar una biblioteca
+
+base::abs(-47) #"Biblioteca" :: "Función"
+
+
