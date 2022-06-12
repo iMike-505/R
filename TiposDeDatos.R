70d277b8a6ff9d60101a8a2aadcc973ab4e35503
Miguel Contreras <79333663+iMike-505@users.noreply.github.com>
2022-06-11 22:01:15 -0500

    Add files via upload

---
+++ TiposDeDatos.R
@@ -0,0 +1,38 @@
+#TIPOS DE DATOS
+
+x <- 23
+class(x) #Class obtiene el tipo de dato de una variable
+
+#Character
+
+y <- "Hello there"
+class(y)
+
+#Boolean
+
+g <- FALSE
+class(FALSE)
+
+#Factor (Variable para crear clasificaciones)
+
+x <- factor("Hombre")
+class(x)
+
+#Date
+
+fecha <- as.Date("2020-09-1")
+class(fecha)
+
+is.numeric(x) #Funciones lógicas
+is.factor(x)
+is.logical(g)
+
+
+
+
+
+
+
+
+
+
