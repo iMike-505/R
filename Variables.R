3143d45536349f827ed192641b976bbbf83e7570
Miguel Contreras <79333663+iMike-505@users.noreply.github.com>
2022-06-11 21:37:09 -0500

    Add files via upload

---
+++ Variables.R
@@ -0,0 +1,34 @@
+#VARIABLES
+
+x <- 4
+x <- 99
+
+x <- "Hello"
+
+y <- "There"
+
+90 -> p #Otra manera (Poco utilizada) para asignar variables
+
+a <- b <- 32 #Se usa más para simplificar
+#Ambos tipos de codigo son validos
+a <- 32
+b <- 32
+
+assign("j", 560)
+
+j
+
+exists("a") #Se usa para verificar si exite esa variable
+exists("c") #C existe como una función
+exists("u")
+
+rm(a) #rm es para borrar variables
+
+ls() #Enlista las variabes asignadas
+
+rm(list = ls()) #Eliminar todas las variables asignadas
+
+
+
+
+
